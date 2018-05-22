d707c676c60b9a3999f6f8268a449bfaddd206a5
Fawn Shao <dreambetter@gmail.com>
2018-05-22 17:13:37 -0500

    chip

--- gene_expression.mutation_rate.R/exp.mut.scatter.plot.R
+++ gene_expression.mutation_rate.R/exp.mut.scatter.plot.R
@@ -13,7 +13,7 @@ for(i in 1:length(targets)){
 		scale_x_continuous(limits = c(0, xmax)) +
 		labs(title = paste(args[1], targets[i], sep = " "), caption = date()) + 
 		theme(axis.text.x = element_text(angle = 60, hjust = 1))
-	png(filename = paste(args[1], targets[i], "scatterplot.png", sep = "."), width = 600, height = 600)
+	png(filename = paste(args[1], targets[i], "scatterplot.png", sep = "."), width = 1000, height = 600)
 	print(myplot)
 	dev.off()
 }
