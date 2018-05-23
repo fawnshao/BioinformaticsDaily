09bd1f05935f9fcd2c21b2c2502f420db49bc4b7
Fawn Shao <dreambetter@gmail.com>
2018-05-23 17:35:08 -0500

    chip

--- gene_expression.mutation_rate.R/exp.mut.boxplot.R
+++ gene_expression.mutation_rate.R/exp.mut.boxplot.R
@@ -10,13 +10,13 @@ for(i in 1:length(targets)){
 	datay <- datax[MutRegions == targets[i],]
 	if(nrow(datay) > 10){
 		ymin <- min(datay$MutCounts)
-		ymax <- quantile(datay$MutCounts, probs = 0.95)
+		ymax <- quantile(datay$MutCounts, probs = 0.9)
 		myplot <- ggplot(data = datay, aes(x = genes, y = MutCounts, fill = ExpressionQuantile)) + 
 			geom_boxplot(position = position_dodge(1), outlier.alpha = 0.1, outlier.size = 0.1) +
 			scale_y_continuous(limits = c(ymin, ymax)) +
 			labs(title = paste(args[1], targets[i], sep = " "), caption = date()) + 
 			theme(axis.text.x = element_text(angle = 60, hjust = 1))
-		png(filename = paste(args[1], targets[i], "boxplot.png", sep = "."), width = 400, height = 600)
+		png(filename = paste(args[1], targets[i], "boxplot.png", sep = "."), width = 600, height = 600)
 		print(myplot)
 		dev.off()
 	}
