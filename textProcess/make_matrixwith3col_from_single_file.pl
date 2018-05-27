c4548abbb5044b792d6a9e3abb0a2d73f6cba6a4
Fawn Shao <dreambetter@gmail.com>
2018-05-27 18:30:20 -0500

    chip

---
+++ textProcess/make_matrixwith3col_from_single_file.pl
@@ -0,0 +1,30 @@
+#!/usr/bin/perl
+%rowlist=();
+%collist=();
+%flag=();
+open(IN, $ARGV[0]);
+while(<IN>){
+	chomp;
+	@t = split(/\t/);
+	$id = $t[0].":".$t[1];
+	$flag{$id} = $t[2];
+	$rowlist{$t[0]}=1;
+	$collist{$t[1]}=1;
+	}
+close(IN);
+
+print "ID";
+foreach $c(sort keys %collist){
+	print "\t$c";
+	}
+print "\n";
+
+foreach $r(keys %rowlist){
+	print "$r";
+	foreach $c(sort keys %collist){
+		$id = $r.":".$c;
+		if(not exists $flag{$id}){$flag{$id} = 0;}
+		print "\t$flag{$id}";
+		}
+	print "\n";
+	}
