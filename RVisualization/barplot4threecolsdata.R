5320e360af7e0f2bf796ad47ee314137b0d7f0cc
Fawn Shao <dreambetter@gmail.com>
2018-05-27 17:51:02 -0500

    chip

--- RVisualization/barplot4threecolsdata.R
+++ RVisualization/barplot4threecolsdata.R
@@ -8,7 +8,7 @@ myplot <- ggplot(data = datax, aes(x = classes, y = values, fill = groups)) +
 	geom_bar(stat = "identity", position = "dodge") + 
 	labs(title = args[1], caption = date()) + 
 	theme(axis.text.x = element_text(angle = 60, hjust = 1))
-png(filename = paste(args[1], "barplot.png", sep = "."), width = 1200, height = 1000)
+png(filename = paste(args[1], "barplot.png", sep = "."), width = 800, height = 600)
 print(myplot)
 dev.off()
 
