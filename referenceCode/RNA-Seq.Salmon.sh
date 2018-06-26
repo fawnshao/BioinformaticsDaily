ec9a42bbc9b404ddba16f4c0c0dc579a27e05253
Fawn Shao <dreambetter@gmail.com>
2018-06-26 17:48:22 -0500

    chip

--- referenceCode/RNA-Seq.Salmon.sh
+++ referenceCode/RNA-Seq.Salmon.sh
@@ -9,8 +9,10 @@
 # wget ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_19/gencode.v19.lncRNA_transcripts.fa.gz
 # zcat gencode.v19.pc_transcripts.fa.gz gencode.v19.lncRNA_transcripts.fa.gz | cat - ERCC92.fa > gencode.v19.ERCC.fa
 # salmon index -p 68 --gencode -t gencode.v19.ERCC.fa -i gencode.v19.ERCC_quasi_index 
+# awk '$3=="transcript"{print $12"\t"$10}' gencode.v19.annotation.gtf | sed 's/"//g;s/;//g' | awk 'BEGIN{print "TXNAME\tGENEID";}{print $0}' > tx2gene.gencode.v19.tsv
 
 salmonIndex=/home1/04935/shaojf/scratch/Salmon.index/gencode.v19.ERCC_quasi_index
+gtf=/home1/04935/shaojf/scratch/Salmon.index/gencode.v19.annotation.gtf
 workDir=/home1/04935/shaojf/scratch/YY1.mut.RNAseq
 fastqDir=$workDir/fastqDir
 salmonDIR=$workDir/salmon.res
@@ -18,10 +20,20 @@ mkdir -p $salmonDIR
 cd $workDir
 
 # salmon quant --help-reads
-
+# --gcBias  
 for f in $fastqDir/*_1.fastq
 do
 	pre=`echo $f | awk -F"/" '{print $NF}' | sed 's/_1.fastq//'`
-	salmon quant -i $salmonIndex -l IU -1 lib_1_1.fq lib_2_1.fq -2 lib_1_2.fq lib_2_2.fq -o out
+	salmon quant -i $salmonIndex -l A \
+	-1 $fastqDir/${pre}_1.fastq -2 $fastqDir/${pre}_2.fastq \
+	-p 68 -o $salmonDIR/$pre.salmon # -g $gtf
 done
 
+echo "samples files" > $workDir/tximport.samples.txt
+for f in $fastqDir/*_1.fastq
+do
+	pre=`echo $f | awk -F"/" '{print $NF}' | sed 's/_1.fastq//'`
+	echo "$pre $salmonDIR/$pre.salmon/quant.sf" >> $workDir/tximport.samples.txt
+done
+
+Rscript 
---
+++ referenceCode/salmon.tximport.RUV.ERCC.R
@@ -0,0 +1,56 @@
+# source("https://bioconductor.org/biocLite.R")
+# biocLite("RUVSeq")
+library(RUVSeq)
+library(RColorBrewer)
+library(tximport)
+library(readr)
+tx2gene <- read_tsv("/home1/04935/shaojf/scratch/Salmon.index/tx2gene.gencode.v19.tsv")
+colors <- brewer.pal(3, "Set2")
+args <- c("tximport.samples.txt", "YY1del")
+input <- read.table(args[1], header = T, sep = " ", row.names = 1)
+myfiles <- as.vector(input$files)
+names(myfiles) <- rownames(input)
+txi <- tximport(myfiles, type = "salmon", tx2gene = tx2gene)
+#### Salmon TPM
+write.table(x = data.frame(rownames(txi$abundance), txi$abundance), 
+	file = paste(args[2],"salmon.tximport.abundance.tsv", sep = "."), 
+	sep = "\t", row.names = F, quote = F)
+estimatecount <- txi$counts
+# filter <- apply(input, 1, function(x) length(x[x>5])>=2)
+# filtered <- zfGenes[filter,]
+# genes <- rownames(filtered)[grep("^ENS", rownames(filtered))]
+# spikes <- rownames(filtered)[grep("^ERCC", rownames(filtered))]
+genes <- rownames(estimatecount)[grep("^ERCC-", rownames(estimatecount), invert = TRUE)]
+spikes <- rownames(estimatecount)[grep("^ERCC-", rownames(estimatecount))]
+x <- as.factor(c(rep("ctrl", 2), rep("YY1del", 2)))
+set <- newSeqExpressionSet(as.matrix(round(estimatecount,0)), 
+	phenoData = data.frame(x, row.names = colnames(estimatecount)))
+# set
+
+set <- betweenLaneNormalization(set, which = "upper")
+set1 <- RUVg(set, spikes, k = 1)
+pData(set1)
+pdf(file = paste(args[2],"RUV.plotRLE.pdf", sep = "."))
+plotRLE(set1, outline = FALSE, ylim = c(-4, 4), col = colors[x])
+dev.off()
+pdf(file = paste(args[2],"RUV.plotPCA.pdf", sep = "."))
+plotPCA(set1, col = colors[x], cex = 1.2)
+dev.off()
+nc <- normCounts(set1)
+write.table(x = data.frame(rownames(nc), nc), 
+	file = paste(args[2],"RUV.normCounts.tsv", sep = "."), 
+	sep = "\t", row.names = F, quote = F)
+
+
+library(DESeq2)
+dds <- DESeqDataSetFromMatrix(countData = counts(set1),
+                               colData = pData(set1),
+                               design = ~ W_1 + x)
+dds <- DESeq(dds)
+res <- results(dds)
+# res
+
+out <- data.frame(counts(dds, normalized = F), counts(dds, normalized = T), res)
+write.table(x = data.frame(rownames(out), out), 
+	file = paste(args[2],"RUV.tsv", sep = "."), 
+	sep = "\t", row.names = F, quote = F)
