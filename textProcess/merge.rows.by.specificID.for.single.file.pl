f498b201e5af26e5c544872b60c1a96435e4f786
Fawn Shao <dreambetter@gmail.com>
2018-07-17 19:12:49 -0500

    chip

---
+++ textProcess/merge.rows.by.specificID.for.single.file.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/perl
+%hash = ();
+open(IN, $ARGV[0]) or die "can not open $ARGV[0]\n";
+while(<IN>){
+	chomp;
+	@t = split(/\t/);
+	$hash{$t[0]} .= $t[1].";";
+}
+close(IN);
+foreach $id(sort keys %hash){
+	print "$id\t$hash{$id}\n";
+}
