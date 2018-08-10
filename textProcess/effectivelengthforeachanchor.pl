12ef7c8117b0c1e03563da3bcefd69818825ee12
Fawn Shao <dreambetter@gmail.com>
2018-08-10 16:35:09 -0500

    chip

---
+++ textProcess/effectivelengthforeachanchor.pl
@@ -0,0 +1,19 @@
+# calculate the effective length for each anchor from bedtools intersect files
+#!/usr/bin/perl
+%hash = ();
+open(IN, $ARGV[0]);
+while(<IN>){
+    chomp;
+    @t = split(/\t/);
+    if($t[11] < 1000) {
+    	$hash{$t[3]} += $t[11];
+    }
+    else{
+    	$hash{$t[3]} += 1000;
+    }
+}
+close(IN);
+
+foreach $anchor(keys %hash){
+	print "$anchor\t$hash{$anchor}\n";
+}
