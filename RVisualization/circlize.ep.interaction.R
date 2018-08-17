a1ee1b455eb0648e78f8b9c20048a4902c4412af
Fawn Shao <dreambetter@gmail.com>
2018-08-17 12:07:38 -0500

    chip

--- RVisualization/circlize.ep.interaction.R
+++ RVisualization/circlize.ep.interaction.R
@@ -1,15 +1,18 @@
 library(circlize)
+library(reshape2)
 args <- commandArgs(TRUE)
-# args <- c("chr1","11166592", "11802263", "subnet",
-#           "11321564", "11323564", "ENSG00000198793.8|MTOR", "HKG", 
-#           "11798028","11802263","enh1", "enh")
-# chr1	11166592	11322564	ENSG00000198793.8|MTOR	.	-
-# chr1:11798028-11802263
+# args <- c("regions","segments","links")
+regions <- read.table(args[1], header = F, sep = "\t")
+segs <- read.table(args[2], header = F, sep = "\t")
+links <- read.table(args[3], header = F, sep = "\t")
 
-df = data.frame(factors = args[1],
-                x = c(as.numeric(args[2:3])))
+df <- melt(regions, id.vars = "V1")
+df <- df[,-2]
+colnames(df) <- c("factors", "x")
 
 circos.clear()
+# png(filename = paste(args[1], "circlize.png", sep = "."), width = 1000, height = 1000)
+
 circos.par("track.height" = 0.1)
 circos.initialize(factors = df$factors, x = df$x)
 
@@ -18,24 +21,20 @@ circos.track(factors = df$factors, ylim = c(0,1),
                circos.text(CELL_META$xcenter, CELL_META$cell.ylim[2] + uy(5, "mm"), 
                            CELL_META$sector.index, cex = 0.8)
                circos.axis(labels.cex = 0.6, 
-                           major.at = df$x[1] + 1e5 * seq(1:floor((df$x[2] - df$x[1]) / 1e5)))
+                           major.at = df$x[1] + 5e5 * seq(1:floor((df$x[2] - df$x[1]) / 5e6)))
              })
 
-for(i in 1:(length(args)/4)){
-    coords <- c(as.numeric(args[(i*4+1):(i*4+2)]))
-    circos.segments(coords[1] ,0.5, coords[2], 0.5, col = factor(args[(i*4+4)]), lwd = 2)
-    circos.text(coords[1], -1, args[(i*4+3)], col = "blue", niceFacing = T, facing = "bending.inside")
+for(i in 1:nrow(segs)){
+    shift <- rnorm(1, mean = 0, sd = 0.1)
+    circos.segments(segs[i,2], 0.5 + shift, segs[i,3], 0.5 + shift, col = as.character(segs[i,5]), lwd = 2)
+    # circos.text(segs[i,3], 1 + shift, segs[i,4], col = as.character(segs[i,5]), cex = 0.6, niceFacing = F, facing = "bending.inside")
 }
-circos.link("seg", 10501, "seg", 16001, h = 0.4)
 
-circos.segments(10201,0.5,10850,0.5, col = factor(), lwd = 2)
-circos.text(10550, -1, "gene1", col = "blue", niceFacing = T, facing = "bending.inside")
+# links[links[,3] < 5,] <- 0
+for(i in 1:nrow(links)){
+    shift <- rnorm(1, mean = 0, sd = 0.1)
+    circos.link(levels(df$factors), links[i,1], levels(df$factors), links[i,2], h = 0.4 + shift, lwd = min(links[i,3], 10))
+}
 
-circos.segments(16001,0.5,17900,0.5, col = "red", lwd = 2)
-circos.text(16550, -1, "gene2", col = "blue", niceFacing = T, facing = "bending.inside")
-
-circos.link("seg", 10501, "seg", 16001, h = 0.4)
-# circos.link("seg", c(10501,10520), "seg", c(15001,15201),
-#             col = "green", border = "black", h = 0.3, lwd = 2, lty = 1)
-
-circos.clear()
+# dev.off()
+# circos.clear()
---
+++ RVisualization/igraph.ep.interaction.R
@@ -0,0 +1,89 @@
+library(igraph)
+args <- commandArgs(TRUE)
+# args <- c("interaction", "nodes")
+
+# triangle vertex shape
+mytriangle <- function(coords, v=NULL, params) {
+    vertex.color <- params("vertex", "color")
+    if (length(vertex.color) != 1 && !is.null(v)) {
+        vertex.color <- vertex.color[v]
+    }
+    vertex.size <- 1/200 * params("vertex", "size")
+    if (length(vertex.size) != 1 && !is.null(v)) {
+        vertex.size <- vertex.size[v]
+    }
+    
+    symbols(x=coords[,1], y=coords[,2], bg=vertex.color,
+            stars=cbind(vertex.size, vertex.size, vertex.size),
+            add=TRUE, inches=FALSE)
+}
+# clips as a circle
+add_shape("triangle", clip=shapes("circle")$clip,
+          plot=mytriangle)
+#################################################################
+# generic star vertex shape, with a parameter for number of rays
+mystar <- function(coords, v=NULL, params) {
+    vertex.color <- params("vertex", "color")
+    if (length(vertex.color) != 1 && !is.null(v)) {
+        vertex.color <- vertex.color[v]
+    }
+    vertex.size  <- 1/200 * params("vertex", "size")
+    if (length(vertex.size) != 1 && !is.null(v)) {
+        vertex.size <- vertex.size[v]
+    }
+    norays <- params("vertex", "norays")
+    if (length(norays) != 1 && !is.null(v)) {
+        norays <- norays[v]
+    }
+    
+    mapply(coords[,1], coords[,2], vertex.color, vertex.size, norays,
+           FUN=function(x, y, bg, size, nor) {
+               symbols(x=x, y=y, bg=bg,
+                       stars=matrix(c(size,size/2), nrow=1, ncol=nor*2),
+                       add=TRUE, inches=FALSE)
+           })
+}
+# no clipping, edges will be below the vertices anyway
+add_shape("star", clip=shape_noclip,
+          plot=mystar, parameters=list(vertex.norays=5))
+#################################################################
+
+links <- read.table(args[1], header = F, sep = "\t")
+vertices <- read.table(args[2], header = F, sep = "\t")
+network <- graph_from_data_frame(d = links, directed = F, vertices = vertices)
+vertices$group <- edge.betweenness.community(network)$membership # betweeness centrality for each node for grouping
+vertices$shape <- rep("circle", nrow(vertices))
+vertices$shape[vertices$V2 == "TSG"] <- "sphere"
+vertices$shape[vertices$V2 == "HKG"] <- "star"
+vertices$shape[vertices$V2 == "Enh"] <- "triangle"
+pdf(file = paste(args[1], "igraph.pdf", sep = "."), width = 20, height = 20)
+plot(network,
+     # mark.groups=vertices$group, # group vertices by betweeness indicator (redish blob background)
+     layout = layout_nicely, #layout.auto, 
+     vertex.color = vertices$group, # color vertices by edge betweeness
+     vertex.label = NA, # no vertex label (name)
+     vertex.size = 5,
+     # edge.arrow.size = 0.8,
+     edge.width = min(links$V3, 5),
+     vertex.shape = vertices$shape)
+dev.off()
+
+##### other analysis
+# # g <- sample_gnp(100, 0.3)
+# clique_num(network)
+# cliques(network, min = 6)
+# largest_cliques(network)
+# 
+# # To have a bit less maximal cliques, about 100-200 usually
+# max_cliques(network)
+# 
+# a <- largest.cliques(network)
+# # let's just take the first of the largest cliques
+# # (in this case there's just one clique)
+# clique1 <- a[[1]]
+# 
+# # subset the original graph by passing the clique vertices
+# g2 <- induced.subgraph(graph = network, vids = clique1)
+# 
+# # plot the clique
+# plot(g2)
