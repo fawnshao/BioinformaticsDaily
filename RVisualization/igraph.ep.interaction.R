cb436756e2b750c3dc373421b4c1c82372017c14
Fawn Shao <dreambetter@gmail.com>
2018-08-17 12:33:03 -0500

    chip

--- RVisualization/igraph.ep.interaction.R
+++ RVisualization/igraph.ep.interaction.R
@@ -1,6 +1,7 @@
 library(igraph)
 args <- commandArgs(TRUE)
 # args <- c("interaction", "nodes")
+# args <- c("interaction.SRR5831489.HKG.links", "interaction.SRR5831489.HKG.links.nodes")
 
 # triangle vertex shape
 mytriangle <- function(coords, v=NULL, params) {
@@ -51,20 +52,25 @@ add_shape("star", clip=shape_noclip,
 links <- read.table(args[1], header = F, sep = "\t")
 vertices <- read.table(args[2], header = F, sep = "\t")
 network <- graph_from_data_frame(d = links, directed = F, vertices = vertices)
-vertices$group <- edge.betweenness.community(network)$membership # betweeness centrality for each node for grouping
+# vertices$group <- edge.betweenness.community(network)$membership # betweeness centrality for each node for grouping
 vertices$shape <- rep("circle", nrow(vertices))
 vertices$shape[vertices$V2 == "TSG"] <- "sphere"
 vertices$shape[vertices$V2 == "HKG"] <- "star"
 vertices$shape[vertices$V2 == "Enh"] <- "triangle"
-pdf(file = paste(args[1], "igraph.pdf", sep = "."), width = 20, height = 20)
+vertices$color <- rep("grey", nrow(vertices))
+vertices$color[vertices$V2 == "TSG"] <- "blue"
+vertices$color[vertices$V2 == "HKG"] <- "red"
+vertices$color[vertices$V2 == "Enh"] <- "green"
+pdf(file = paste(args[1], "igraph.pdf", sep = "."), width = 50, height = 50)
 plot(network,
      # mark.groups=vertices$group, # group vertices by betweeness indicator (redish blob background)
-     layout = layout_nicely, #layout.auto, 
-     vertex.color = vertices$group, # color vertices by edge betweeness
+     layout = layout.auto, #layout_nicely, 
+     # vertex.color = vertices$group, # color vertices by edge betweeness
+     vertex.color = vertices$color,
      vertex.label = NA, # no vertex label (name)
-     vertex.size = 5,
+     vertex.size = 1,
      # edge.arrow.size = 0.8,
-     edge.width = min(links$V3, 5),
+     edge.width = min(links$V3, 5)/5,
      vertex.shape = vertices$shape)
 dev.off()
 
